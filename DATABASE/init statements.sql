41b95524e4b96489deffd876b4365696fd901bb1
cappelleMedia <cappelle.design@gmail.com>
2016-04-21 11:16:02 +0200

    added files

---
+++ DATABASE/init statements.sql
@@ -0,0 +1,44 @@
+-- souffe_reviews mysql stuff
+
+-- images
+INSERT INTO `images` (`image_id`, `img_uri`, `alt`) VALUES (NULL, 'avatar_dummy1.jpg', 'avatar dummy one'), (NULL, 'avatar_dummy2.jpg', 'avatar dummy two');
+INSERT INTO `images` (`image_id`, `img_uri`, `alt`) VALUES (NULL, 'achievement_dummy1.jpg', 'achievement dummy one'), (NULL, 'achievement_dummy2.jpg', 'achievement dummy two'), (NULL, 'header_dummy1.jpg', 'header dummy one'), (NULL, 'gallery_dummy1.jpg', 'gallery dummy one'), (NULL, 'gallery_dummy2.jpg', 'gallery dummy two'), (NULL, 'gallery_dummy3.jpg', 'gallery dummy three'), (NULL, 'gallery_dummy4.jpg', 'gallery dummy four'), (NULL, 'gallery_dummy5.jpg', 'gallery dummy five'), (NULL, 'newsfeed_dummy1.jpg', 'newsfeed dummy one'), (NULL, 'newsfeed_dummy2.jpg', 'newsfeed dummy two');
+-- avatars 
+INSERT INTO `avatars` (`avatar_id`, `images_image_id`, `tier`) VALUES (NULL, '1', '1'), (NULL, '2', '1');
+-- user roles
+INSERT INTO `user_roles` (`user_role_id`, `user_role_name`, `user_role_access_flag`, `user_role_karma_min`, `user_role_diamond_min`) VALUES (NULL, 'Noob', '1', '0', '0'), (NULL, 'freshmen', '2', '15', '0');
+-- users
+INSERT INTO `users` (`user_id`, `avatars_avatar_id`, `user_roles_user_role_id`, `user_name`, `user_hash_salt`, `user_email`, `user_karma`, `user_reg_key`, `user_warnings`, `user_diamonds`, `preference_datetime_format`, `user_created`, `last_login`, `donated_amount`, `active_seconds`) VALUES (NULL, '1', '1', 'jens', '$2y$10$1lxrzaC0jwVa5nI8m/3hCezY65JKQ0XLxUDX282007kf44qBxy70K', 'jens@localhost.be', '0', '84PP1gZf1WPA5R390KJNNEGRFbK1CrtKviAxyzNAvCaBtdLuYvhL9YCOamQM', '0', '0', 'd/m/Y H:i:s', '2016-03-14 00:00:00', '2016-03-14 00:00:00', '0', '0'), (NULL, '2', '2', 'jens2', '$2y$10$1lxrzaC0jwVa5nI8m/3hCezY65JKQ0XLxUDX282007kf44qBxy70K', 'jens2@localhost.be', '16', '84PP1gZf1WPA5R390KJNNEGRFbK1CrtKviAxyzNAvCaBtdLuYvhL9YCOamQM', '0', '0', 'd/m/Y H:i:s', '2016-03-01 00:00:00', '2016-03-01 00:00:00', '0', '18000');
+-- achievements
+INSERT INTO `achievements` (`achievement_id`, `image_id`, `name`, `desc`, `karma_award`, `diamond_award`) VALUES (NULL, '3', 'achievement first', 'Achievement unlocked for being a dummy tester. that''s what this is.. a dummy', '10', '1'), (NULL, '4', 'achievement second', 'The second dummy achievement.. My dummy hahaa', '120', '5');
+-- achievements_users
+INSERT INTO `achievements_users` (`user_id`, `achievements_id`) VALUES ('1', '1'), ('2', '1'), ('2', '2');
+-- games
+INSERT INTO `games` (`game_id`, `game_name`, `game_release`, `game_website`, `game_publisher`, `game_developer`, `min_online_players`, `max_online_players`, `max_offline_players`, `story_max_players`, `has_storymode`) VALUES (NULL, 'game1', '2016-03-01', 'wwww.game1.com', 'Bethesda', 'Infinity Ward', '2', '16', '4', '4', '1');
+-- genres
+INSERT INTO `genres` (`genre_id`, `genre_name`, `genre_description`) VALUES (NULL, 'genre1', 'description about this genre'), (NULL, 'genre2', 'description about this genre');
+-- platforms
+INSERT INTO `platforms` (`platform_id`, `platform_name`) VALUES (NULL, 'Playstation 4'), (NULL, 'Xbox One');
+-- game genre
+INSERT INTO `game_genre` (`game_id`, `genre_id`) VALUES ('1', '1'), ('1', '2');
+-- game platform
+INSERT INTO `game_platform` (`game_id`, `platform_id`) VALUES ('1', '1'), ('1', '2');
+-- newsfeeds 
+INSERT INTO `newsfeeds` (`newsfeed_id`, `writer_id`, `image_id`, `newsfeed_subject`, `newsfeed_body`, `newsfeed_created`) VALUES (NULL, '1', '11', 'Dummy subject one', 'This is a newsfeed text and might become TXT field btw.. not sure if this has to be long or nog', '2016-03-02 00:00:00'), (NULL, '1', '12', 'dummy newsfeed 2', 'The text of this dummy hereeeee the newsfeed of your life gangsta', '2016-03-02 00:00:00');
+-- notifications
+INSERT INTO `notifications` (`notification_id`, `user_id`, `notification_txt`, `notification_date`, `notification_isread`) VALUES (NULL, '1', 'The first notification everrrrrrrrr. much important, very wow', '2016-03-14 00:00:00', '0'), (NULL, '2', 'The first notification everrrrrrrrr. much important, very wow', '2016-03-01 00:00:00', '0');
+-- reviews
+INSERT INTO `reviews` (`review_id`, `users_writer_id`, `game_id`, `platforms_platform_id`, `review_title`, `review_score`, `review_txt`, `review_video_url`, `review_created`) VALUES (NULL, '1', '1', '1', 'Game1: The good, the bad and the ugly', '8', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque tincidunt at quam vitae rhoncus. Suspendisse sollicitudin urna ut ipsum congue, sit amet ornare nisi scelerisque. Integer efficitur, dolor a dapibus malesuada, nibh felis mattis lorem, eu volutpat magna orci quis dolor. Fusce eu scelerisque dui. Donec facilisis mi orci, non finibus purus faucibus ac. Sed vel nulla ultrices tellus semper ultricies sit amet eu ante. Integer eros eros, tristique at sollicitudin ut, finibus ut sapien. Morbi vitae eros non ex mattis efficitur. Integer vitae urna lacus. Nam vestibulum nunc eu ante maximus, eget gravida urna posuere.Vestibulum diam tellus, dignissim et luctus vitae, posuere ultricies mi. Curabitur et urna sit amet lectus tincidunt maximus. Vivamus erat neque, fermentum et elit id, scelerisque sollicitudin sem. Praesent quis libero eleifend, tincidunt est vel, vulputate metus. Suspendisse dui purus, finibus vel neque a, sollicitudin consectetur nibh. Curabitur facilisis tempor varius. Praesent ac turpis non justo accumsan eleifend. Duis sed porta sem. Nullam blandit sit amet neque vel volutpat. Ut pretium mauris id neque condimentum pellentesque. Quisque nisi quam, lacinia id semper sit amet, convallis in tortor. Maecenas ac urna sed lectus lacinia congue. Etiam sagittis tristique orci, id congue magna vehicula et. Suspendisse facilisis suscipit arcu id elementum. Sed porttitor consequat dolor ut pharetra.', 'https://www.youtube.com/watch?v=dRjE1JwdDLI', '2016-03-14 17:33:49');
+-- reviews images
+INSERT INTO `reviews_has_images` (`reviews_review_id`, `images_image_id`, `headerpic`) VALUES ('1', '5', '1'), ('1', '6', '0'), ('1', '7', '0'), ('1', '8', '0'), ('1', '9', '0'), ('1', '10', '0');
+-- review user scores
+INSERT INTO `review_user_scores` (`review_id`, `user_id`, `score`) VALUES ('1', '2', '8');
+-- review votes
+INSERT INTO `review_votes` (`users_user_id`, `reviews_review_id`, `vote_flag`) VALUES ('1', '1', '1'), ('2', '1', '2');
+-- comments
+INSERT INTO `comments` (`comment_id`, `users_writer_id`, `review_id`, `parent_id`, `comment_txt`, `comment_created`) VALUES (NULL, '1', '1', '1', 'Much review, such wow!', '2016-03-01 00:00:00');
+INSERT INTO `comments` (`comment_id`, `users_writer_id`, `review_id`, `parent_id`, `comment_txt`, `comment_created`) VALUES (NULL, '2', '1', '1', 'yeah buddy', '2016-03-01 00:00:12');
+-- comment votes
+INSERT INTO `comment_votes` (`comment_id`, `users_upvoter_id`, `vote_flag`) VALUES ('1', '2', '2'), ('2', '1', '2');
+-- good bad tags
+INSERT INTO `goods_bads_tags` (`good_bad_tag_id`, `review_id`, `good_bad_tag_txt`, `good_bad_tag_type`) VALUES (NULL, '1', 'very nice game indeed', 'good'), (NULL, '1', 'very nice graphics', 'good'), (NULL, '1', 'i did not see tits', 'bad'), (NULL, '1', 'game1', 'tag'), (NULL, '1', 'nice grapics', 'tag');
---
+++ Honest_Reviews_mindmap.mm
@@ -0,0 +1,93 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1450362980899" ID="ID_1884025846" MODIFIED="1450363763791" TEXT="WEB: Honest Reviews">
+<node CREATED="1450363050367" HGAP="110" ID="ID_609116444" MODIFIED="1450366060721" POSITION="right" STYLE="bubble" TEXT="PAGE: Home" VSHIFT="736">
+<icon BUILTIN="bookmark"/>
+<node CREATED="1450363121835" HGAP="11" ID="ID_387481245" MODIFIED="1450363570964" STYLE="fork" TEXT="CONTENT1: Most recent Review highlighted" VSHIFT="41">
+<icon BUILTIN="full-1"/>
+</node>
+<node CREATED="1450363323213" HGAP="9" ID="ID_1648818853" MODIFIED="1450363577174" STYLE="fork" TEXT="CONTENT2: 3 random &apos;articles&apos; (review or streams) in form of little box which links to page" VSHIFT="11">
+<icon BUILTIN="full-2"/>
+</node>
+<node CREATED="1450363599487" HGAP="8" ID="ID_1838378437" MODIFIED="1450363765431" STYLE="fork" TEXT="CONTENT3: Newsfeed block with all newsfeed items from past x months + link to all newsfeeds" VSHIFT="15">
+<icon BUILTIN="full-3"/>
+<node CREATED="1450363652703" HGAP="12" ID="ID_720575885" MODIFIED="1450364019362" STYLE="bubble" TEXT="PAGE: Link to all newsfeeds leads to newsfeeds page" VSHIFT="34">
+<icon BUILTIN="password"/>
+<node CREATED="1450363849329" HGAP="13" ID="ID_198960166" MODIFIED="1450364022630" STYLE="fork" TEXT="CONTENT1: list of all newsfeeds" VSHIFT="37">
+<icon BUILTIN="full-1"/>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1450363822669" HGAP="87" ID="ID_1940533084" MODIFIED="1450366063543" POSITION="right" STYLE="bubble" TEXT="PAGE: About" VSHIFT="-14">
+<icon BUILTIN="bookmark"/>
+<node CREATED="1450363991845" ID="ID_1259788773" MODIFIED="1450364012488" STYLE="fork" TEXT="CONTENT NOT YET KNOWN">
+<icon BUILTIN="help"/>
+</node>
+</node>
+<node CREATED="1450364081541" HGAP="85" ID="ID_1955480029" MODIFIED="1450366071123" POSITION="right" STYLE="bubble" TEXT="PAGE: Reviews" VSHIFT="-89">
+<icon BUILTIN="bookmark"/>
+<node CREATED="1450364156197" HGAP="11" ID="ID_504680506" MODIFIED="1450364527747" STYLE="fork" TEXT="CONTENT1: Overview of reviews " VSHIFT="37">
+<arrowlink DESTINATION="ID_504680506" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_703629943" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<linktarget COLOR="#b0b0b0" DESTINATION="ID_504680506" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_703629943" SOURCE="ID_504680506" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<icon BUILTIN="full-1"/>
+<node CREATED="1450364214029" HGAP="21" ID="ID_679054069" MODIFIED="1450364572770" TEXT="ATTRIBUTES1: User can chose to only see games with specific Values=&gt;" VSHIFT="24">
+<icon BUILTIN="forward"/>
+<node CREATED="1450364249427" ID="ID_366841249" MODIFIED="1450364478440" TEXT="Platform(only xbox or ps4 etc...)"/>
+<node CREATED="1450364262133" ID="ID_1368387906" MODIFIED="1450364443991" TEXT="Game type(only action or shooter etc...)"/>
+<node CREATED="1450364285975" ID="ID_909271368" MODIFIED="1450364471231" TEXT="Number of players (only single player, 1-2, 1-4, etc..)"/>
+<node CREATED="1450364315212" ID="ID_285297801" MODIFIED="1450364504400" TEXT="Rating (only show games with minimum score of &apos;X&apos;"/>
+</node>
+<node CREATED="1450364561557" ID="ID_1005060240" MODIFIED="1450364618920" TEXT="ATTRIBUTES2: User can chose to sort games on name, platform, etc..(see above for options)">
+<icon BUILTIN="forward"/>
+</node>
+<node CREATED="1450364681448" ID="ID_139897754" MODIFIED="1450365245422" TEXT="ATTRIBUTES3: A Review &apos;block&apos; has(at least) the game name, the score and a background picture" VSHIFT="33">
+<icon BUILTIN="forward"/>
+</node>
+<node CREATED="1450364756759" HGAP="21" ID="ID_611826492" MODIFIED="1450366090272" STYLE="bubble" TEXT="PAGE: When a review is clicked it wil lead to a &apos;review specific&apos; page" VSHIFT="-15">
+<icon BUILTIN="password"/>
+<node CREATED="1450364787564" HGAP="11" ID="ID_844549940" MODIFIED="1450366099829" STYLE="fork" TEXT="CONTENT1: This page contains all the aspects of the game (name, platform, release, score,..." VSHIFT="29">
+<icon BUILTIN="full-1"/>
+</node>
+<node CREATED="1450364889517" HGAP="13" ID="ID_1064344899" MODIFIED="1450366099827" STYLE="fork" TEXT="CONENT2: The reviews &apos;goods&apos; and &apos;bads&apos; are listed here (this is are small &apos;bullet-points&apos; of the positive and negative aspects of the game)" VSHIFT="6">
+<icon BUILTIN="full-2"/>
+</node>
+<node CREATED="1450364926514" HGAP="13" ID="ID_1999609267" MODIFIED="1450366099829" STYLE="fork" TEXT="CONTENT3: An imbeded youtube frame will show the video review" VSHIFT="6">
+<icon BUILTIN="full-3"/>
+</node>
+<node CREATED="1450365060037" HGAP="13" ID="ID_684798774" MODIFIED="1450366099827" STYLE="fork" TEXT="CONTENT4: The Review text is displayed here as well" VSHIFT="9">
+<icon BUILTIN="full-4"/>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1450364145871" HGAP="13" ID="ID_1684036669" MODIFIED="1450366079932" POSITION="right" STYLE="bubble" TEXT="PAGE: Streams" VSHIFT="31">
+<icon BUILTIN="bookmark"/>
+<node CREATED="1450365556165" HGAP="12" ID="ID_514800729" MODIFIED="1450366083139" STYLE="fork" TEXT="CONTENT1: 2 tabs (1 for twitch streams, 1 for Youtube gaming streams; could become more in time)" VSHIFT="37">
+<icon BUILTIN="full-1"/>
+</node>
+<node CREATED="1450365630629" HGAP="10" ID="ID_239905309" MODIFIED="1450366085845" STYLE="fork" TEXT="CONTENT2: video &apos;box&apos; (imbeded twitch/yt frame) witch comment section under it" VSHIFT="8">
+<icon BUILTIN="full-2"/>
+</node>
+</node>
+<node CREATED="1450365832401" HGAP="17" ID="ID_902832908" MODIFIED="1450366115601" POSITION="right" STYLE="bubble" TEXT="PAGE: Donate" VSHIFT="14">
+<icon BUILTIN="bookmark"/>
+<node CREATED="1450365895101" HGAP="13" ID="ID_1391130749" MODIFIED="1450366124077" STYLE="fork" TEXT="CONTENT1: Simple motivation text to make people donate" VSHIFT="47">
+<icon BUILTIN="full-1"/>
+</node>
+<node CREATED="1450365975775" HGAP="12" ID="ID_820844513" MODIFIED="1450366124077" STYLE="fork" TEXT="CONTENT2: paypal donation form" VSHIFT="3">
+<icon BUILTIN="full-2"/>
+</node>
+</node>
+<node CREATED="1450366016276" HGAP="21" ID="ID_1140047791" MODIFIED="1450366347180" POSITION="right" STYLE="bubble" TEXT="PAGE: Contact" VSHIFT="-868">
+<icon BUILTIN="bookmark"/>
+<node CREATED="1450366166581" HGAP="16" ID="ID_580151538" MODIFIED="1450366351401" STYLE="fork" TEXT="CONTENT1: Social media buttons (fb, twitch, twitter, youtube, ..?)" VSHIFT="31">
+<icon BUILTIN="full-1"/>
+</node>
+<node CREATED="1450366245723" ID="ID_7342575" MODIFIED="1450366351400" STYLE="fork" TEXT="CONTENT2: Contact formulier (3 fields: email address, name and message)">
+<icon BUILTIN="full-2"/>
+</node>
+</node>
+<node CREATED="1450366362695" ID="ID_1992459153" MODIFIED="1450366396408" POSITION="left" TEXT="user login icon to let a user log in -&gt; sends user to login screen"/>
+</node>
+</map>
